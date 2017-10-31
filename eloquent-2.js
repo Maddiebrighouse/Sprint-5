115a5fa7a82228e3b75d6e57999fb0312fb409c2
madeleine-brighouse <31881309+Maddiebrighouse@users.noreply.github.com>
2017-11-01 10:30:51 +1300

    task 5.1

---
+++ eloquent-2.js
@@ -0,0 +1,17 @@
+//Looping a triangle//
+for (var number = "#"; number.length <= 7; number += "#")
+  console.log(number);
+
+//FizzBuzz//
+for (var number = 0; number <= 100; number = number + 1)
+  if (number % 3 == 0 && number % 5 == 0){
+    console.log("FizzBuzz");
+  } else if (number % 3 == 0){
+    console.log ("Fizz");
+  } else if (number % 5 == 0){
+    console.log("Buzz");
+  }else {
+    console.log(number);
+  }
+  //Chess board//
+//Didnt manage to complette this task.
